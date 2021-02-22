cbcd434a0a5839daebe46553757dce9393a4f456
Marco Martins <marcodanielmartins@gmail.com>
2021-02-22 16:29:34 +0100

    Tests for collections and schemas

--- src/__tests__/_mocks.ts
+++ src/__tests__/_mocks.ts
@@ -1,8 +1,13 @@
 import nock = require('nock')
 import { Scope } from 'nock'
+import got from 'got'
 
-export const mockRequest = (url: string, result: Record<string, unknown>): Scope =>
+export const mockClient = got.extend({
+    prefixUrl: process.env.COCKPIT_API_URL,
+    responseType: 'json',
+})
+
+export const mockRequest = (url: string, result: string | Record<string, any>): Scope =>
     nock(process.env.COCKPIT_API_URL || '')
         .get(`/${url}`)
         .reply(200, result)
-        .persist()
--- src/__tests__/http.test.ts
+++ src/__tests__/http.test.ts
@@ -1,24 +1,20 @@
 import test from 'ava'
 import nock from 'nock'
-import { mockRequest } from './_mocks'
 import { get } from '../http'
 import got from 'got'
 
 test.afterEach(() => nock.cleanAll())
 
-test('success http get call with modifier response', async (t) => {
+test('http get call with success result and modifier response', async (t) => {
     const mockReturnData = { content: 'my content' }
 
-    mockRequest('collections/get/myCollection', mockReturnData)
+    nock(process.env.COCKPIT_API_URL || '')
+        .get(`/collections/get/myCollection`)
+        .reply(200, mockReturnData)
 
     const client = got.extend({
         prefixUrl: process.env.COCKPIT_API_URL,
-        headers: {
-            'Content-Type': 'application/json',
-            'Cockpit-Token': process.env.COCKPIT_API_TOKEN,
-        },
         responseType: 'json',
-        mutableDefaults: true,
     })
 
     const result = await get<{ content: string }>('collections/get/myCollection')(client)
@@ -27,5 +23,17 @@ test('success http get call with modifier response', async (t) => {
 
     if (result.success) {
         t.deepEqual(result.data, mockReturnData)
+    }
+})
+
+test('http get call with non success result and error message', async (t) => {
+    const client = got.extend({ responseType: 'json' })
+
+    const result = await get('collections/get/myCollection')(client)
+
+    t.false(result.success)
+
+    if (!result.success) {
+        t.is(result.message, 'TypeError [ERR_INVALID_URL]: Invalid URL: collections/get/myCollection')
     }
 })
