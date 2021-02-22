d81d32602d9709c12e64c871bf3de00f4fd54eb3
Marco Martins <marcodanielmartins@gmail.com>
2021-02-22 12:35:05 +0100

    Use return types for schema

--- src/cockpitCollections.ts
+++ src/cockpitCollections.ts
@@ -12,7 +12,7 @@ export type CockpitCollectionEntries = {
     entriesFn: (name: string) => ResponseResult<CockpitCollectionEntry>
 }
 
-export const cockpitCollectionEntries = async <T>({ list, entriesFn }: CockpitCollectionEntries) =>
+export const cockpitCollectionEntries = async <T>({ list, entriesFn }: CockpitCollectionEntries): Promise<T> =>
     ((await list.data.reduce(async (entries: Promise<{ [n: string]: CockpitCollectionEntry }>, name) => {
         const result = await entriesFn(name)
         if (result.success) {
@@ -26,7 +26,7 @@ export const cockpitCollectionEntries = async <T>({ list, entriesFn }: CockpitCo
 
 export const cockpitCollections = (client: Got) => ({
     list: () => get<string[]>(`collections/listCollections`)(client),
-    schemas: () => get<Schema[]>(`collections/listCollections/extended`)(client),
-    schema: (id: string) => get<Schema>(`collections/collection/${id}`)(client),
+    schemas: <T>() => get<Schema[] | T>(`collections/listCollections/extended`)(client),
+    schema: <T>(id: string) => get<Schema | T>(`collections/collection/${id}`)(client),
     entries: <T>(id: string) => get<Entries<T>>(`collections/entries/${id}?populate=5`)(client),
 })
